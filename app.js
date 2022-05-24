c6f4c7b1e67b2b3f3a121d2fa3989d84568824d0
Van toan <vantoanvo26@gmail.com>
2022-05-25 00:30:08 +0700

    ok

---
+++ app.js
@@ -0,0 +1,42 @@
+// var courseAPI='http://localhost:3000/courses';
+
+// fetch(courseAPI)
+// .then(function(response){
+//     return response.json();
+// })
+// .then(function(courses){
+//     console.log(courses)
+// })
+
+var listCoursesBlock = document.querySelector("#list-courses");
+
+var courseAPI = "http://localhost:3000/courses";
+
+function start() {
+    
+//   getCourses(function (courses) {
+//     renderCourses(courses)
+//   });
+getCourses(renderCourses())
+}
+
+start();
+
+function getCourses(callback) {
+  fetch(courseAPI)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(callback);
+}
+
+function renderCourses(courses){
+    var listCoursesBlock = document.querySelector("#list-courses");
+    var htmls=courses.map(function(courses){
+        return `
+          <li>
+            <h4>${$courses.name}</h4>
+          </li>
+        `;
+    })
+}
